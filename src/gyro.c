8f5cc405561c6ee00cd1f74c98e2f27162967753
Shekhar Karani <aryan.karani2005@gmail.com>
2025-11-04 09:55:38 -0500

    Rollbacked

--- src/gyro.c
+++ src/gyro.c
@@ -3,7 +3,7 @@
 
 const int I2C_GYRO_SDA = 28;
 const int I2C_GYRO_SCL = 29;
-const int INTERRUPT_GYRO_PIN = 27; //TODO
+const int INTERRUPT_GYRO_PIN = 27; // TODO
 
 extern int16_t gyro[3];
 
@@ -43,7 +43,8 @@ void gyro_init()
     gyro_intr_init();
 }
 
-void gyro_intr_init() {
+void gyro_intr_init()
+{
     uint8_t buffer[] = {0x38, 0x01};
     i2c_write_timeout_us(i2c0, addr, buffer, 2, false, 0);
     gpio_add_raw_irq_handler(INTERRUPT_GYRO_PIN, read_gyro);
@@ -56,25 +57,28 @@ void gyro_intr_init() {
 void gyro_reset()
 {
     uint8_t buffer[] = {0x6B, 0x80};
-    i2c_write_timeout_us(i2c0, addr, buffer, 2, false, 0);    
+    i2c_write_timeout_us(i2c0, addr, buffer, 2, false, 0);
     sleep_ms(100);
 
     buffer[1] = 0x00;
     i2c_write_timeout_us(i2c0, addr, buffer, 2, false, 0);
     sleep_ms(10);
 }
-    
-void read_gyro() {
+
+void read_gyro()
+{
     uint8_t buffer[6];
     uint8_t val = 0x43;
 
     gpio_acknowledge_irq(INTERRUPT_GYRO_PIN, gpio_get_irq_event_mask(INTERRUPT_GYRO_PIN) & GPIO_IRQ_EDGE_RISE);
-    
+
     int res = i2c_write_timeout_us(i2c0, addr, &val, 1, true, 0);
-    if (res < 0) return res;
+    if (res < 0)
+        return res;
     i2c_read_raw_blocking(i2c0, buffer, 6);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
     printf("Gyro Values: %d, %d, %d\n", gyro[0], gyro[1], gyro[2]);
